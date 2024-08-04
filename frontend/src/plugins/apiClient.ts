9a5fb5768a9400fc4f7433a6164b999edcfd419f
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-08-04 14:58:51 -0300

    fix: update api client

--- frontend/src/plugins/apiClient.ts
+++ frontend/src/plugins/apiClient.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 const apiUrl = process.env.REACT_APP_API_URL;
-console.log(apiUrl)
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: apiUrl,
     withCredentials: false,
     headers: {
         'Content-Type': 'application/json',
